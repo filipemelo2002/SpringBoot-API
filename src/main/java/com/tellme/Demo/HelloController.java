e6c84c8bfabf6a8b501d4c4c5459b23bacb597a4
Filipe Melo <filipe.melo@liferay.com>
2021-11-28 14:16:41 -0300

    Add JSON response

--- src/main/java/com/tellme/Demo/HelloController.java
+++ src/main/java/com/tellme/Demo/HelloController.java
@@ -3,11 +3,21 @@ package com.tellme.Demo;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestController
 public class HelloController {
 
     @GetMapping("/")
     public String index() {
         return "Greetings from Spring Boot!";
+    }
+
+    @GetMapping("/json")
+    public Map<String, String> jsonResponse() {
+        Map<String, String> map = new HashMap<>();
+        map.put("message", "Greetings from Spring Boot, in JSON!");
+        return map;
     }
 }
